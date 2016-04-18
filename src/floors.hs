6331e41d256052e0ac18d5cde73c521ac03ea72d
Miguel Fontes <miguel_m_fontes@hotmail.com>
2016-04-18 17:12:57 -0300

    Ajuste em bloco Where da Função hitTheBasement

--- src/floors.hs
+++ src/floors.hs
@@ -4,12 +4,12 @@ floors :: String -> Int
 floors [] = 0
 floors (x:xs) = move x + floors xs
 
-hitTheBasement :: String -> Int -> Int -> Int
-hitTheBasement [] _ i = i
-hitTheBasement (x:xs) n i
-    | n == (-1) = i
-    | otherwise = hitTheBasement xs f (i+1)
-        where f = n + move x
+hitTheBasement :: String -> Int
+hitTheBasement x = floorsIter x 0 0
+    where floorsIter [] _ i = i
+          floorsIter (x:xs) n i
+              | n == (-1) = i
+              | otherwise = floorsIter xs (n + move x) (i+1)
 
 move :: Char -> Int
 move x
